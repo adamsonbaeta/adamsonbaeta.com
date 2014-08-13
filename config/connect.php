197e99b48e4cfb5f440691e88cc3b87dc0a86ffe
adamsonbaeta <adamsonbaeta@gmail.com>
2014-08-13 13:10:08 +0800

    linking CSS

---
+++ config/connect.php
@@ -0,0 +1,9 @@
+<?php
+	
+	//define('DB_HOST', '64.64.2.124');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'webd1la_hrm');
+	define('DB_PASS', 'AGSI$$$');
+	define('DB_NAME', 'webd1la_contacta_hrm');
+	
+	
